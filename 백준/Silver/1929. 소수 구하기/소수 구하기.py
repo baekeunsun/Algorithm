efedeebcce5f9a5be050e71d2de701455a53f952
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-01-03 02:07:10 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 38092 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,15 @@
-def primeNumber(N,M):
-    for now in range(N,M+1):
-        if now > 1:
-            sieve = True
-            for i in range(2, int(now ** 0.5) + 1):
-                if now%i == 0:
-                    sieve = False
-                    break
-            if sieve:
-                print(now)
+m, n = map(int, input().split())
 
-N,M = map(int,input().split())
-primeNumber(N,M)
+def isprime(m, n):
+  n += 1                            # for문 사용을 위한 n += 1
+  prime = [True] * n                # n개의 [True]가 있는 리스트 생성
+  for i in range(2, int(n**0.5)+1): # n의 제곱근까지만 검사
+    if prime[i]:                    # prime[i]가 True일때
+      for j in range(2*i, n, i):    # prime 내 i의 배수들을 False로 변환
+        prime[j] = False
+
+  for i in range(m, n):
+    if i > 1 and prime[i] == True:  # 1 이상이면서 남은 소수들을 출력
+      print(i)
+
+isprime(m, n)
