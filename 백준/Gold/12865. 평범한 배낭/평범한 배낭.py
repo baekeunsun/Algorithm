aecfa120b6e4a4a2f7d5258fd4905302386e16b5
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-01-08 19:09:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 4672 ms, Memory: 281632 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,20 +1,15 @@
-n, k = map(int,input().split())
+N, K = map(int,input().split())
+load = [[0,0]] + [list(map(int,input().split())) for _ in range(N)]
+dp = [[0]*(K+1) for _ in range(N+1)]
 
-st = [[0,0]]
-snap = [[0]*(k+1) for _ in range(n+1)]
+for i in range(1,N+1):
+    weight = load[i][0]
+    value = load[i][1]
+    
+    for nowWeight in range(1,K+1) :
+        if nowWeight < weight :
+            dp[i][nowWeight] = max(dp[i-1][nowWeight], dp[i][nowWeight-1])
+        else :
+            dp[i][nowWeight] = max(value + dp[i-1][nowWeight-weight], dp[i-1][nowWeight])
 
-for _ in range(n):
-    st.append(list(map(int,input().split())))
-
-for i in range(1,n+1):
-    for j in range(1,k+1):
-        weight = st[i][0]
-        value = st[i][1]
-        
-        if j < weight:
-            snap[i][j] = max(snap[i-1][j], snap[i][j-1])
-            
-        else:
-            snap[i][j] = max(value + snap[i-1][j-weight] , snap[i-1][j])
-
-print(snap[n][k])
+print(dp[N][K])
