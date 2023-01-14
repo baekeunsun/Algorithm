4c25e65c3b3d1fe89d7d145556b1f0e22123366c
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-01-14 21:45:51 +0900

    [Gold V] Title: 벼락치기, Time: 396 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,15 +1,10 @@
 N,T = map(int,input().split())
-study = [[0,0]] + [list(map(int,input().split())) for _ in range(N)]
-dp =[[0]*(T+1) for _ in range(N+1)]
+dp =[0]*(T+1)
 
-for i in range(1,N+1):
-    time = study[i][0]
-    score = study[i][1]
+for _ in range(N):
+    time, score = map(int,input().split())
 
-    for j in range(1,T+1) :
-        if j < time :
-            dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
-        else :
-            dp[i][j] = max(score + dp[i-1][j-time], dp[i-1][j])
+    for nowTime in range(T,time-1,-1) :
+        dp[nowTime] = max(dp[nowTime], dp[nowTime-time]+score)
 
-print(dp[N][T])
+print(dp[T])
