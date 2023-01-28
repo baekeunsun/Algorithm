148503a8ec04a451a137bd3e6296d60a3b79d782
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-01-29 02:55:08 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,19 +1,29 @@
 def solution(babbling):
     can = {"a":"aya", "y":"ye", "w":"woo", "m":"ma"}
     answer = 0
-    for i in range(len(babbling)):
-        now = babbling[i]
+    for now in babbling:
         while now : # now 문자있는 동안
             if now[0] in can :  # 해당하는 단어 시작이 있을 경우
                 if now[:len(can[now[0]])] == can[now[0]] :
                     now = now.replace(can[now[0]],"")   # 대체
                     if now :
                         continue
-                    else :
-                        answer += 1
+                    answer += 1
+                    break
                 else :
                     break
             else :
                 break
         continue
     return answer
+
+
+def solution(babbling):
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
