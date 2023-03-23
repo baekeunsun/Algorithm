eb3e1497c2c62d8e47200f86080110f9379e068d
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-03-23 22:21:05 +0900

    [Platinum V] Title: 히스토그램, Time: 152 ms, Memory: 43300 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -0,0 +1,21 @@
+# 히스토그램 
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []  # 인덱스, 값
+finish = 0 # 인덱스들의 차(가로) * 값(세로)
+
+for i in range(N):
+    value = int(input())
+    start = i
+    while stack and stack[-1][1] > value:
+        start, preValue = stack.pop()
+        finish = max(finish,(i-start)*preValue)
+    stack.append([start,value])
+
+while stack :
+    start, preValue = stack.pop()
+    finish = max(finish,(N-start)*preValue)
+
+print(finish)
