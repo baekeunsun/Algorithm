7096872b6afe81da6d49c2849b2303a9ba7fd084
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-03-24 16:23:52 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 252 ms, Memory: 47160 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -4,21 +4,18 @@ input = sys.stdin.readline
 
 def histogram(N, read):
     stack = []
-    finish = 0 # 인덱스들의 차(가로) * 값(세로)
+    answer = 0
+    read.append(0)
 
-    for i in range(N):
+    for i in range(N+1):
         value = read[i]
         start = i
-        while stack and stack[-1][1] > value:
-            start, preValue = stack.pop()
-            finish = max(finish,(i-start)*preValue)
-        stack.append([start,value])
-
-    while stack :
-        start, preValue = stack.pop()
-        finish = max(finish,(N-start)*preValue)
-
-    print(finish)
+        while stack and stack[-1][1] >= value :
+            start, height = stack.pop()
+            answer = max(answer, (i-start)*height)
+        stack.append([start, value])
+    
+    print(answer)
 
 while True :
     read = list(map(int,input().split(' ')))
