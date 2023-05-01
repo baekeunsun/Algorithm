8efeb3daa9ef56b187fe8d9749dd41bdb7e08a62
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-01 16:09:14 +0900

    [Gold I] Title: 영훈이의 색칠공부, Time: 2344 ms, Memory: 35268 KB -BaekjoonHub

---
+++ 백준/Gold/14578. 영훈이의 색칠공부/영훈이의 색칠공부.py
@@ -0,0 +1,17 @@
+# 영훈이의 색칠공부
+N = int(input())
+dp = [0,0,1]
+fact = 2
+mod = 1000000007
+
+if N > 2 :
+    for i in range(3,N+1):
+        dp.append(((dp[i-1]+dp[i-2])*(i-1))%mod)
+        fact *= i
+    print((fact*dp[N])%mod)
+    
+else :
+    if N == 1:
+        print(0)
+    else :
+        print(2)
