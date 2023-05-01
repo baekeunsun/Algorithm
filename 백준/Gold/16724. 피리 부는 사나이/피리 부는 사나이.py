ffb3c9cc2e08e372561fa26975c7ee4429dcce8d
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-01 20:51:27 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 712 ms, Memory: 78440 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -2,7 +2,7 @@
 N, M = map(int,input().split())
 Map = list(list(map(str,input())) for _ in range(N))
 
-group = [[-1 for _ in range(M)] for _ in range(N)]
+visit = [[-1 for _ in range(M)] for _ in range(N)]
 
 direction = ['L','R','U','D']
 dx = [0,0,-1,1]
@@ -10,12 +10,12 @@ dy = [-1,1,0,0]
 
 def move(x,y,idx):
     global answer
-    if group[x][y] != -1:   # 방문함
-        if group[x][y] == idx:
+    if visit[x][y] != -1:   # 방문함
+        if visit[x][y] == idx:
             answer += 1
         return  # 만일 형성된 사이클에 방문하더라도 idx가 다르기때문에 괜찮다
 
-    group[x][y] = idx
+    visit[x][y] = idx
     i = direction.index(Map[x][y])
     move(x + dx[i], y + dy[i], idx)
 
