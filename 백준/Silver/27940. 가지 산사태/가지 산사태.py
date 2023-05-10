09808b1161f3350ed1de2a50586db1e789fdf0b4
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-10 20:54:47 +0900

    [Silver V] Title: 가지 산사태, Time: 504 ms, Memory: 117980 KB -BaekjoonHub

---
+++ 백준/Silver/27940. 가지 산사태/가지 산사태.py
@@ -0,0 +1,21 @@
+# 가지 산사태
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int,input().split())   # 농장층수, 비 횟수, 버틸수있는 빗물 양
+farm = [0]*(N+1)
+
+total = 0
+for i in range(1,M+1):
+    t, r = map(int,input().split())  # 1층~t층, r만큼 비
+    farm[t] += r
+    total += r
+    if total > K :
+        tmp = 0
+        for j in range(N,0,-1):
+            tmp += farm[j]
+            if tmp > K :
+                print(i, j)
+                sys.exit()
+    
+print(-1)
