2f6f6f6f587825834536ab56dc538ac1e4c30357
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-14 20:54:28 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,29 +1,19 @@
 # 암호 만들기
 # 암호 조건 : a,e,i,o,u 중 최소 두개 /  알파벳 오름차순
-import itertools
+from itertools import combinations
 
 L, C = map(int,input().split())
-string = list(map(str,input().split()))
-
-ja = []
-mo = []
-
-for i in string :
-    if i in 'aeiou' :
-        ja.append(i)
-    else :
-        mo.append(i)
-
+string = sorted(list(map(str,input().split())))
 answer = []
-tmp = []
-for n in range(1,min(len(ja)+1, L-1)):
-    for case1 in (itertools.combinations(ja, n)) :
-        for case2 in (itertools.combinations(mo, L - n)):
-            tmp = list(case1 + case2)
-            tmp.sort()
-            answer.append(tmp)
-answer.sort()
 
+for word in (combinations(string, L)):
+    count = 0
+    for w in word :
+        if w in 'aeiou' :
+            count += 1
+    if count >= 1 and (L-count) >= 2 :
+        answer.append(list(word))
+        
 for i in answer :
     print(*i, sep='')
     
