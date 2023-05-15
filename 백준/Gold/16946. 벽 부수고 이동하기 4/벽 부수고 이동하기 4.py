ae4e55d90e399e030a732cc490c49a77f7128331
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-15 18:37:57 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 704 ms, Memory: 179376 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -15,20 +15,20 @@ def bfs(sx, sy):
     
     while queue :
         x, y = queue.popleft()
-        if visit[x][y] == True :       
-            visit[x][y] = False
-            count += 1
-            Map[x][y] = group_cnt
-            for i in range(4):
-                ix = x + dx[i]
-                iy = y + dy[i]
-                if N > ix >= 0 and M > iy >= 0 and Map[ix][iy] == 0:
-                    queue.append([ix,iy])
+        count += 1
+        Map[x][y] = group_cnt
+        for i in range(4):
+            ix = x + dx[i]
+            iy = y + dy[i]
+            if N > ix >= 0 and M > iy >= 0 and Map[ix][iy] == 0 and visit[ix][iy] == True:
+                queue.append([ix,iy])
+                visit[ix][iy] = False
     return count
 
 for i in range(N):
     for j in range(M):
         if Map[i][j] == 0 : # 그룹 생성
+            visit[i][j] = True
             group.append(bfs(i,j))
             group_cnt += 1
             
