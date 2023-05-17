7d3c02f336b32a894e2ffdb440c80431a868064a
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-05-17 18:10:44 +0900

    [Silver IV] Title: 2+1 세일, Time: 104 ms, Memory: 35964 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -1,15 +1,16 @@
 # 2+1 세일
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 buy = list(int(input()) for _ in range(N))
-buy.sort()
-answer = 0
+buy.sort(reverse=True)
+answer = []
 
 for i in range(N//3):
-    answer += buy.pop()
-    answer += buy.pop()
-    buy.pop()
+    answer += buy[i*3:i*3+2]
     
-while buy :
-    answer += buy.pop()
+for j in range(N%3):
+    answer.append(buy[-1-j])
 
-print(answer)
+print(sum(answer))
