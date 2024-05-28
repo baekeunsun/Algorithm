930b8a004ab03ab550b32233ee4ee19d579def72
백은선 <72916415+baekeunsun@users.noreply.github.com>
2024-05-28 18:06:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,12 +1,28 @@
 N = int(input())
-i = 1
-num = 666
+i = 0
 
-while True:
-    if i == N :
-        break
-    num += 1
-    if '666' in str(num) :
-        i += 1
-
-print(num)
+while N > 0:
+    if i % 1000 == 666:
+        for j in range(1000):
+            N -= 1
+            if N == 0:
+                print(i * 1000 + j)
+                break
+    elif i % 100 == 66:
+        for j in range(100):
+            N -= 1
+            if N == 0:
+                print(i * 1000 + 600 + j)
+                break
+    elif i % 10 == 6:
+        for j in range(10):
+            N -= 1
+            if N == 0:
+                print(i * 1000 + 660 + j)
+                break
+    else:
+        N -= 1
+        if N == 0:
+            print(i * 1000 + 666)
+            break
+    i += 1
